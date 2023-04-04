5ba5afdb0b12de9f3f8f6c8804c11d4cceea4636
shenjy <shenjy24@gmail.com>
2023-04-04 16:01:24 +0800

    单链表的分解

---
+++ src/main/java/com/jonas/list/PartitionList.java
@@ -0,0 +1,62 @@
+package com.jonas.list;
+
+import com.jonas.util.ListNode;
+import com.jonas.util.ListUtil;
+
+/**
+ * 单链表的分解（leetcode: 86）
+ */
+public class PartitionList {
+
+    public static ListNode partition(ListNode head, int x) {
+        // 存放小于 x 的链表的虚拟头结点
+        ListNode dummy1 = new ListNode(-1);
+        // 存放大于等于 x 的链表的虚拟头结点
+        ListNode dummy2 = new ListNode(-1);
+        // p1, p2 指针负责生成结果链表
+        ListNode p1 = dummy1, p2 = dummy2;
+        // p 负责遍历原链表，类似合并两个有序链表的逻辑
+        // 这里是将一个链表分解成两个链表
+        ListNode p = head;
+        while (p != null) {
+            if (p.val >= x) {
+                p2.next = p;
+                p2 = p2.next;
+            } else {
+                p1.next = p;
+                p1 = p1.next;
+            }
+            // 断开原链表中的每个节点的 next 指针
+            ListNode temp = p.next;
+            p.next = null;
+            p = temp;
+        }
+        // 连接两个链表
+        p1.next = dummy2.next;
+        return dummy1.next;
+    }
+
+    public static ListNode buildList() {
+        ListNode head = new ListNode(1);
+        ListNode node2 = new ListNode(4);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(2);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(2);
+
+        head.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = buildList();
+        ListUtil.printList(head);
+        head = partition(head, 3);
+        ListUtil.printList(head);
+    }
+}
