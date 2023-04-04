cf79f0f347317d4dfc306e71370a8fc6e2934ce8
shenjy <shenjy24@gmail.com>
2023-04-04 16:10:43 +0800

    链表中间结点

--- src/main/java/com/jonas/list/MiddleNode.java
+++ src/main/java/com/jonas/list/MiddleNode.java
@@ -7,7 +7,24 @@ import com.jonas.util.ListUtil;
  * 单链表的中点 (leetcode:876)
  */
 public class MiddleNode {
+
+    /**
+     * 如果结点数为偶数，则取中间两个结点的前面一个
+     */
     public static ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    /**
+     * 如果结点数为偶数，则取中间两个结点的后面一个
+     */
+    public static ListNode middleNode2(ListNode head) {
         ListNode slow = head;
         ListNode fast = head.next;
         while (fast != null && fast.next != null) {
