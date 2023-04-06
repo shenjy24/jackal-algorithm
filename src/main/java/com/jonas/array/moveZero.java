2b1529f20140482fc498b5de3589a5bfbae77a48
Jonas <shenjy24@gmail.com>
2023-04-06 21:10:50 +0800

    移动0

---
+++ src/main/java/com/jonas/array/moveZero.java
@@ -0,0 +1,29 @@
+package com.jonas.array;
+
+import com.jonas.util.ArrayUtil;
+
+/**
+ * 移动零 (leetcode: 283)
+ */
+public class moveZero {
+
+    public static void moveZeroes(int[] nums) {
+        int slow = 0, fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != 0) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        for (int i = slow; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        moveZeroes(nums);
+        ArrayUtil.printArray(nums);
+    }
+}
